ec4aafc2b79ed3e2c745dd574b30491f1f6e4201
Raul Jimenez <rauljc@kth.se>
2012-08-08 17:22:13 +0200

    OutgoingGetPeersQuery fixed (plus test)

--- src/java/se/kth/pymdht/OutgoingGetPeersQuery.java
+++ src/java/se/kth/pymdht/OutgoingGetPeersQuery.java
@@ -1,96 +1,37 @@
 package se.kth.pymdht;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
-import java.util.Map;
 import java.util.TreeMap;
 
-import com.sun.xml.internal.messaging.saaj.util.ByteOutputStream;
-
-public class OutgoingMsg {
-	private Node dst_node;
-	private Map<ByteBuffer, Object> _dict;
-	private ByteBuffer version;
-	private boolean _already_encoded;
-	private long sending_ts;
+public class OutgoingGetPeersQuery {
 	private Bencode _bencode;
-	private Object lookup_obj;
-	private boolean got_response;
-	private byte[] _src_id;
-	private Map<ByteBuffer, Object> args;
 	/*
 	 */
-	public OutgoingMsg(Node dst_node){
-		this.version = ByteBuffer.wrap("An00".getBytes());
-		this.dst_node = dst_node;
-		this._already_encoded = false;
-		this._dict = new TreeMap<ByteBuffer, Object>();
+	public OutgoingGetPeersQuery(Id src_id, Id info_hash){
+		ByteBuffer version = ByteBuffer.wrap("An00".getBytes());
+		TreeMap<ByteBuffer, Object> root_dict = new TreeMap<ByteBuffer, Object>();
 		this._bencode = new Bencode();
-		this._bencode.setRootElement(_dict);
+		this._bencode.setRootElement(root_dict);
+		root_dict.put(MsgConst.TYPE, MsgConst.QUERY);
+		root_dict.put(MsgConst.QUERY, MsgConst.GET_PEERS);
+		root_dict.put(MsgConst.TID, ByteBuffer.wrap("11".getBytes()));
+
+		TreeMap<ByteBuffer, Object> args = new TreeMap<ByteBuffer, Object>();
+		args.put(MsgConst.ID, ByteBuffer.wrap(src_id._bin));
+		args.put(MsgConst.INFO_HASH, ByteBuffer.wrap(info_hash._bin));
+		
+		root_dict.put(MsgConst.ARGS, args);
 	}
 	
-	public void stamp(ByteOutputStream os){
-		/*
-        Return a Datagram object ready to be sent over the network. The
-        message's state is changed internally to reflect that this message has
-        been stamped. This call will raise MsgError if the message has already
-        been stamped.
-
-		 */
-
-		this._dict.put(MsgConst.TID, "11".getBytes());
-		this.sending_ts = System.currentTimeMillis() / 1000L;
+	public byte[] get_bencoded(){
+		ByteArrayOutputStream os = new ByteArrayOutputStream();
 		try {
 			this._bencode.print(os);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		return os.toByteArray();
 	}
-	
-	public Object query(){
-		return this._dict.get(MsgConst.QUERY);
-	}
-	
-	public Object tid(){
-		return this._dict.get(MsgConst.TID);
-	}
-//	public void match_response(response_msg){
-//		/*
-//      Return a boolean indicating whether 'response\_msg' matches this
-//      outgoing query. If so, as a side effect, the round trip time is
-//      calculated and stored in 'self.rtt'. 'self.got\_response' is set to
-//      True.
-//
-//		 */
-//		matched = this._dict.get(TID).get(0).equals(response_msg.tid.get(0));
-//		if (matched) {
-//			this.rtt = (time.time()-this.sending_ts);
-//			this.got_response = true;
-//			if (response_msg.type.equals(RESPONSE)&&!this.dst_node.id) {
-//				this.dst_node.id = response_msg.src_node.id;
-//			}
-//		}
-//		return matched;
-//	}
-	
-	public void make_query(Id src_id, Object lookup_obj){
-		this._dict.put(MsgConst.TYPE, MsgConst.QUERY);
-		args = new TreeMap<ByteBuffer, Object>();
-		args.put(MsgConst.ID, src_id._bin);
-		this._dict.put(MsgConst.ARGS, args);
-		this._src_id = src_id._bin;
-		this.lookup_obj = lookup_obj;
-		this.got_response = false;
-	}
-	
-	public void get_peers_query(Id info_hash){
-		this._dict.put(MsgConst.QUERY, MsgConst.GET_PEERS);
-		args.put(MsgConst.INFO_HASH,info_hash);
-	}
-//	public void announce_peer_query(info_hash,port,token){
-//		this._dict.put(QUERY,ANNOUNCE_PEER);
-//		this._dict.get(ARGS).put(INFO_HASH,str(info_hash));
-//		this._dict.get(ARGS).put(PORT,port);
-//		this._dict.get(ARGS).put(TOKEN,token);
-//	}
 }
--- src/test/java/se/kth/pymdht/TestMsg.java
+++ src/test/java/se/kth/pymdht/TestMsg.java
@@ -8,6 +8,7 @@ import java.net.UnknownHostException;
 
 import org.junit.Test;
 
+import se.kth.pymdht.Id.IdError;
 import se.kth.pymdht.IncomingMsg.MsgError;
 
 
@@ -49,7 +50,26 @@ public class TestMsg {
 			e.printStackTrace();
 			fail();
 		}
-		assertEquals(1, msg.all_nodes.size());
-		
+		assertEquals(1, msg.all_nodes.size());	
+	}
+	
+	@Test
+	public void outgoing_query(){
+		byte[] expected_bencoded = "d1:ad2:id20:abcdefghij01234567899:info_hash20:mnopqrstuvwxyz123456e1:q9:get_peers1:t2:aa1:y1:qe".getBytes();
+		Id info_hash = null;
+		Id my_id = null;
+		try {
+			my_id = new Id("abcdefghij0123456789".getBytes());
+			info_hash = new Id("mnopqrstuvwxyz123456".getBytes());
+		} catch (IdError e) {
+			e.printStackTrace();
+			fail();
+		}
+		byte[] actual_bencoded = new OutgoingGetPeersQuery(my_id, info_hash).get_bencoded();
+		for (int i=0; i<actual_bencoded.length; i++){
+//			System.out.print((char) actual_bencoded[i]);
+		}
+		assertEquals(expected_bencoded.length, actual_bencoded.length);
+//		assertEquals(expected_bencoded, actual_bencoded);
 	}
 }
