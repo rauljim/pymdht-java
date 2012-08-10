1b2b02812d713243c4e96c1d9744f88100d492df
Raul Jimenez <rauljc@kth.se>
2012-08-10 10:51:37 +0200

    added overlay bootstrap

--- src/java/se/kth/pymdht/LookupQueue.java
+++ src/java/se/kth/pymdht/LookupQueue.java
@@ -1,6 +1,7 @@
 package se.kth.pymdht;
 
 import java.net.Inet4Address;
+import java.net.InetSocketAddress;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
@@ -10,32 +11,29 @@ import java.util.TreeSet;
 
 class LookupQueue {
 
+	private static final int REPLICA_SIZE = 3;
 	private SortedSet<QueuedNode> queue;
 	private SortedSet<QueuedNode> closest_responded_qnodes;
 	private Set<Inet4Address> queued_ips;
-	private Set<Inet4Address> queried_ips;
 	private Id info_hash;
+	private List<InetSocketAddress> stable_addrs;
+	private List<InetSocketAddress> unstable_addrs;
 	
-	public LookupQueue(Id info_hash){
+	public LookupQueue(Id info_hash, List<InetSocketAddress> unstable_addrs, List<InetSocketAddress> stable_addrs){
 		this.info_hash = info_hash;
+		this.unstable_addrs = unstable_addrs;
+		this.stable_addrs = stable_addrs;
 		this.queue = new TreeSet<QueuedNode>();
 		this.queued_ips = new HashSet<Inet4Address>();
-		this.queried_ips = new HashSet<Inet4Address>();
 		this.closest_responded_qnodes = new TreeSet<QueuedNode>();
 		
 	}
 	
-	public List<Node> bootstrap(List<Node> nodes){
-		for (Node node : nodes){
-			this.queried_ips.add(node.ip);
-		}
-		return nodes;
-	}
-	
-	public List<Node> on_response(Node src_node, List<Node> nodes, int max_nodes){
+	public void on_response(Node src_node, List<Node> nodes){
 		QueuedNode qnode = new QueuedNode(src_node,
 				src_node.id.distance(this.info_hash));
-		if (this.closest_responded_qnodes.size() > 0){
+		assert this.closest_responded_qnodes.size() <= REPLICA_SIZE;
+		if (this.closest_responded_qnodes.size() == REPLICA_SIZE){
 			QueuedNode last = this.closest_responded_qnodes.last();
 			if (qnode.distance.log < last.distance.log){
 				this.closest_responded_qnodes.remove(last);
@@ -49,31 +47,35 @@ class LookupQueue {
 				queue.add(qnode);
 			}
 		}
-		return _pop_nodes_to_query(max_nodes);
 	}
 		
-	public List<Node> on_timeout(int max_nodes){
-		return _pop_nodes_to_query(max_nodes);
-	}
-		
-	private List<Node> _pop_nodes_to_query(int max_nodes){
-		List<Node> nodes_to_query = new ArrayList<Node>();
-		
-		int mark = Id.ID_SIZE_BITS;
-		if (this.closest_responded_qnodes.size() > 0){
-			mark = this.closest_responded_qnodes.last().distance.log;
+	public List<Node> get_nodes_to_query(){
+		ArrayList<Node> nodes_to_query = new ArrayList<Node>();
+
+		if (this.queue.size() > 0){
+			QueuedNode candidate_qnode = this.queue.first();
+			if (this.closest_responded_qnodes.size() < REPLICA_SIZE ||
+					candidate_qnode.distance.compareTo(this.closest_responded_qnodes.last().distance) < 0){
+				this.queue.remove(candidate_qnode);
+				nodes_to_query.add(candidate_qnode.node);
+			}
 		}
-		QueuedNode qnode;
-		while (this.queue.size() > 0 && nodes_to_query.size() < max_nodes){
-			qnode = this.queue.first();
-			this.queue.remove(qnode);
-			if (qnode.distance.log < mark){
-				this.queried_ips.add(qnode.node.ip);
-				nodes_to_query.add(qnode.node);
-				queued_ips.remove(qnode.node.ip);
+		else{
+			Node node;
+			if (this.unstable_addrs.size() > 0){
+				int num_nodes = Math.min(this.unstable_addrs.size(), GetPeersLookup.NUM_UNSTABLE_PER_ROUND);
+				for (int i=0; i<num_nodes; i++){
+					node = new Node(this.unstable_addrs.remove(0));
+					nodes_to_query.add(node);
+				}
+			}
+			else{
+				if (this.stable_addrs.size() > 0){
+					node = new Node(this.unstable_addrs.remove(0));
+					nodes_to_query.add(node);
+				}
 			}
 		}
 		return nodes_to_query;
-	}
-	
+	}	
 }
--- src/test/java/se/kth/pymdht/TestLookup.java
+++ src/test/java/se/kth/pymdht/TestLookup.java
@@ -9,11 +9,48 @@ import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import se.kth.pymdht.Id.IdError;
 
 public class TestLookup{
+	Id info_hash=null, id1=null, id2=null, id3=null, id4 = null;
+	Node node1=null, node2=null, node3=null, node4 = null;
+	LookupQueue lq;
+	
+	@Before
+	public void setUp(){
+		try {
+			info_hash = new Id("0000000000000000000000000000000000000000");
+			id1 = new Id("1000000000000000000000000000000000000000");
+			id2 = new Id("2000000000000000000000000000000000000000");
+			id3 = new Id("3000000000000000000000000000000000000000");
+			id4 = new Id("4000000000000000000000000000000000000000");
+		} catch (IdError e) {
+			e.printStackTrace();
+			fail();
+		}
+		try {
+			node1 = new Node(new InetSocketAddress(Inet4Address.getByName(
+					"1.1.1.1"), 1111), id1);
+			node2 = new Node(new InetSocketAddress(Inet4Address.getByName(
+					"1.1.1.2"), 2222), id2);
+			node3 = new Node(new InetSocketAddress(Inet4Address.getByName(
+					"1.1.1.3"), 3333), id3);
+			node4 = new Node(new InetSocketAddress(Inet4Address.getByName(
+					"1.1.1.4"), 4444), id4);
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+			fail();
+		}
+		List<InetSocketAddress> unstable = new ArrayList<InetSocketAddress>();
+		unstable.add(node3.addr);
+		unstable.add(node4.addr);
+		List<InetSocketAddress> stable = new ArrayList<InetSocketAddress>();
+
+		lq = new LookupQueue(info_hash, unstable, stable);
+	}
 
 	@Test
 	public void qnode(){
@@ -31,49 +68,27 @@ public class TestLookup{
 	
 	@Test
 	public void lookup_queue(){
-		Id info_hash=null, id1=null, id2=null, id3=null, id4 = null;
-		try {
-			info_hash = new Id("0000000000000000000000000000000000000000");
-			id1 = new Id("1000000000000000000000000000000000000000");
-			id2 = new Id("2000000000000000000000000000000000000000");
-			id3 = new Id("3000000000000000000000000000000000000000");
-			id4 = new Id("4000000000000000000000000000000000000000");
-		} catch (IdError e) {
-			e.printStackTrace();
-			fail();
-		}
-		Node node1=null, node2=null, node3=null, node4 = null;
-		try {
-			node1 = new Node(new InetSocketAddress(Inet4Address.getByName(
-					"1.1.1.1"), 1111), id1);
-			node2 = new Node(new InetSocketAddress(Inet4Address.getByName(
-					"1.1.1.2"), 2222), id2);
-			node3 = new Node(new InetSocketAddress(Inet4Address.getByName(
-					"1.1.1.3"), 3333), id3);
-			node4 = new Node(new InetSocketAddress(Inet4Address.getByName(
-					"1.1.1.4"), 4444), id4);
-		} catch (UnknownHostException e) {
-			e.printStackTrace();
-			fail();
-		}
-		LookupQueue lq = new LookupQueue(info_hash);
 		
-		List<Node> nodes = new ArrayList<Node>(2);
-		nodes.add(node3);
-		nodes.add(node4);
-		List<Node> nodes_to_query = lq.bootstrap(nodes);
-		assertEquals(nodes.size(), nodes_to_query.size());
-		assertEquals(nodes, nodes_to_query);
+		List<Node> nodes_to_query = lq.get_nodes_to_query();
+		assertEquals(2, nodes_to_query.size());
+		assertEquals(node3.addr, nodes_to_query.get(0).addr);
+		assertEquals(node4.addr, nodes_to_query.get(1).addr);
 		
 		ArrayList<Node> found_nodes = new ArrayList<Node>(0);
-		nodes_to_query = lq.on_response(node3, found_nodes, 5);
+		lq.on_response(node3, found_nodes);
+		nodes_to_query = lq.get_nodes_to_query();
 		assertEquals(0, nodes_to_query.size());
 
 		found_nodes.add(node2);
 		found_nodes.add(node1);
-		nodes_to_query = lq.on_response(node4, found_nodes, 5);
-		assertEquals(2, nodes_to_query.size());
+		lq.on_response(node4, found_nodes);
+		nodes_to_query = lq.get_nodes_to_query();
+		assertEquals(1, nodes_to_query.size());
 		assertEquals(node1, nodes_to_query.get(0));
-		assertEquals(node2, nodes_to_query.get(1));
-	}
+		nodes_to_query = lq.get_nodes_to_query();
+		assertEquals(1, nodes_to_query.size());
+		assertEquals(node2.addr, nodes_to_query.get(0).addr);
+		nodes_to_query = lq.get_nodes_to_query();
+		assertEquals(0, nodes_to_query.size());
+}
 }
